ab1178845922fd04d9a5e4186b0c165f0228dd04
DARRIGOValentin <34090762+DARRIGOValentin@users.noreply.github.com>
2018-02-18 17:34:12 +0100

    3col vers XCSP3
    j'ai ajouté des fonctions permettant d'écrire un fichier au format XCSP3 correspondant au problème 3col du graphe
    
    j'ai écrit une fonction dans la classe main pour testé ce que j'ai fait
    
    maintenant pour testé le fichier XCSP3 il faut un solver
    j'ai choisi celui ci (choco-parsers-4.0.4-with-dependencies.jar)
    https://github.com/chocoteam/choco-parsers/releases/tag/choco-parsers-4.0.4
    (https://github.com/chocoteam/choco-parsers)
    
    après il faut taper la commande suivante :
    java -cp choco-parsers-4.0.4-with-dependencies.jar org.chocosolver.parser.xcsp.ChocoXCSP fichier_XCSP3

--- src/structures/Graph.java
+++ src/structures/Graph.java
@@ -97,4 +97,79 @@ public class Graph {
 		}
 		return str;
 	}
+	
+	/*les fonctions en dessous servent à écrire un problème 3-col au format XCSP3
+	 * et à partir de ce format on résoudre ce prolème avec un solver adéquat */
+	
+	/*
+	 * retourne une chaine de caractère representant un fichier au format XCSP3
+	 * correspodant au problème 3-col du graphe
+	 * */
+	public String TreeColToXCSP3(){
+		StringBuilder str = new StringBuilder();
+		str.append("<instance  format=\"XCSP3\" type=\"CSP\">\r\n");
+		appendVariablesFor3ColToXCSP(str);
+		appendConstraintsFor3ColToXCSP(str);
+		str.append("</instance>");
+		return str.toString();
+	}
+	
+	/*
+	 * ecrit un fichier au format XCSP3 correspodant au problème 3-col du graphe
+	 * vers le fichier f dans le répertoire courrant
+	 */
+	public void export3ColToXCSP3(File f) {
+		try {
+			FileWriter writer = new FileWriter(f);
+			writer.write(TreeColToXCSP3());
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * ajoute la valise <var> correspondant au sommet à la chaine de carractère
+	 * */
+	private void appendVarFor3ColToXCSP(StringBuilder str, int vertex){
+		str.append("\t\t<var id=\"v"+vertex+"\"> 0 1 2 </var>\r\n");
+	}
+	
+	/*
+	 * ajoute la valise <variables> correspondant aux sommets à la chaine de carractère
+	 * */
+	private void appendVariablesFor3ColToXCSP(StringBuilder str){
+		str.append("\t<variables>\r\n");
+		
+		for (int i =0; i < nbVertex; i++)
+			appendVarFor3ColToXCSP(str, i);
+		
+		str.append("\t</variables>\r\n");
+	}
+	
+	/*
+	 * ajoute la valise <extension> correspondant a une arête à la chaine de carractère
+	 * */
+	private void appendExtensionFor3ColToXCSP(StringBuilder str, Edge edge){
+		str.append("\t\t<extension>\r\n");
+		
+		str.append("\t\t\t<list>v"+edge.getVertex1() + " v" + edge.getVertex2() +"</list>\r\n");
+		
+		str.append("\t\t\t<supports>(0,1)(0,2)(1,0)(1,2)(2,0)(2,1)</supports>\r\n");
+		
+		str.append("\t\t</extension>\r\n");
+	}
+	
+	/*
+	 * ajoute la valise <constraints> correspondant aux arêtes à la chaine de carractère
+	 * */
+	private void appendConstraintsFor3ColToXCSP(StringBuilder str){
+		str.append("\t<constraints>\r\n");
+		
+		for (Edge edge : edges)
+			appendExtensionFor3ColToXCSP(str, edge);
+		
+		str.append("\t</constraints>\r\n");
+	}
 }
