a9543c65b287857d377a22e2128890d2b6901d55
Unknown <adrien.varet@etu.univ-amu.fr>
2018-04-12 15:31:33 +0200

    Support Encoding

--- src/reductions/CSPToSat.java
+++ src/reductions/CSPToSat.java
@@ -34,24 +34,52 @@ public class CSPToSat {
 			}
 		}
 		
+		clauses.add("c encoding constraitns");
+		
 		/* encoding constraints */
 		for (Constraint constraint : csp.getConstraints()) {
 			Variable v1 = constraint.getV1();
 			Variable v2 = constraint.getV2();
 			int v1IndexBase = Integer.parseInt(v1.getName().substring(1));
 			int v2IndexBase = Integer.parseInt(v2.getName().substring(1)); 
-			ArrayList<String> v1Values = new ArrayList<String>();
-			ArrayList<String> v2Values = new ArrayList<String>();
+			
+			ArrayList<String> values1 = new ArrayList<String>();
+			ArrayList<String> values2 = new ArrayList<String>();
+			
 			for (Couple c1 : constraint.getRelation().getCouples()) {
+				String firstValue1 = c1.getValue1();
+				String firstValue2 = c1.getValue2();
 				for (Couple c2 : constraint.getRelation().getCouples()) {
 					if (!c1.equals(c2)) {
-						
+						if (!c2.getValue1().equals(firstValue1)) {
+							if (!values1.contains(c2.getValue2()))
+								values1.add(c2.getValue2());
+						} else {
+							values1.remove(c2.getValue2());
+						}
+						/*
+						if (!c2.getValue2().equals(firstValue2)) {
+							values2.add(c2.getValue2());
+						} else {
+							values2.remove(c2.getValue1());
+						}*/
 					}
 				}
+				//v2IndexBase * v2.getDomain().getSize() + Integer.parseInt(couple.getValue2()) + 1;
+				String clause = new String();
+				clause += "-" + (v1IndexBase * v1.getDomain().getSize() + Integer.parseInt(firstValue1) + 1) + " ";
+				for (String value : values1) {
+					clause += (v2IndexBase * v1.getDomain().getSize() + Integer.parseInt(value) + 1) + " ";
+				}
+				clause += "0";
+				clauses.add(clause);
+				
+				values1.clear();
+				values2.clear();
 			}
 		}
 		
-		return null;
+		return new SatFNC(csp.getNbVariables() * csp.getDomains().get(0).getSize(), clauses.size(), clauses);
 	}
 	
 	public static SatFNC directEncoding(BinCSP csp) {
@@ -92,5 +120,11 @@ public class CSPToSat {
 		}
 		
 		return new SatFNC(litteral - 1, clauses.size(), clauses);
+	}
+	
+	public static void main(String [] args) {
+		BinCSP csp = BinCSP.importFromXML("test3col.xml");
+		SatFNC sat = supportEncoding(csp);
+		System.out.println(sat.toString());
 	}
 }
